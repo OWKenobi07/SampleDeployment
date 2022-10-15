7282ca4cd9ea5964c827d43ceff8283c5f85cb5c
OWKenobi07 <37969650+OWKenobi07@users.noreply.github.com>
2022-10-15 11:37:47 +0800

    with Route

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 import Home from './Home';
-import EnrolleeList from './EnrolleeList';
+// import EnrolleeList from './EnrolleeList';
 import EnrollForm from './EnrollForm';
-import UserRegistration from './UserRegistration';
-import StudentApplicants from './StudentApplicants';
+// import UserRegistration from './UserRegistration';
+// import StudentApplicants from './StudentApplicants';
 
 
 const Routes = () => {
@@ -17,7 +17,7 @@ const Routes = () => {
                  <Route exact strict path = '/EnrollForm' component={EnrollForm} render = {() => <EnrolleeList/>}/>
                  <Route exact strict path = '/UserRegistration' component={UserRegistration} render = {()=> <UserRegistration />} />
                  <Route exact strict path = '/StudentApplicants' component={StudentApplicants} render = {()=> <StudentApplicants/>} /> */}
-                 <Route exact strict path = '/EnrollForm' component={EnrollForm} render = {() => <EnrolleeList/>}/>
+                 <Route exact strict path = '/EnrollForm' component={EnrollForm} render = {() => <EnrollForm/>}/>
             </Switch>
         
         </>
