a59f5526ebcf5772eb4240891eedf9a92ef32764
OWKenobi07 <37969650+OWKenobi07@users.noreply.github.com>
2022-10-15 12:02:30 +0800

    antd

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import {HashRouter as Router} from 'react-router-dom'
+
+import "antd/dist/antd.css";
 import './index.css';
 import App from './App';
-import {HashRouter as Router} from 'react-router-dom'
 
 ReactDOM.render(
   <React.Fragment>
