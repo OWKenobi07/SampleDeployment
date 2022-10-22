9c8b400d9511155c810a581d67458035d1e02a36
OWKenobi07 <37969650+OWKenobi07@users.noreply.github.com>
2022-10-22 09:24:19 +0800

    with webpack

--- src/index.js
+++ src/index.js
@@ -7,12 +7,14 @@ import "antd/dist/antd.css";
 import './index.css';
 import App from './App';
 
+const appElement = document.getElementById('app');
+
 ReactDOM.render(
   <React.Fragment>
       <Router>
         <App />
       </Router>
   </React.Fragment>,
-  document.getElementById('root')
+  appElement
 );
 
---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+
+const path = require('path')
+const tailwindcss = require('tailwindcss');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+
+    entry: path.resolve(__dirname, './src/index.js'),
+  
+    devServer:{
+        port: 3000,
+        hot: true,
+        open: true,
+        historyApiFallback: true     
+    },
+
+    optimization: {
+      splitChunks: {
+        chunks:"all"
+        }
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            base: '/',
+            template: './src/index.html',
+          }),
+         
+
+    ],
+
+    module :{
+            rules: [
+                {
+                  test: /\.(j|t)sx?$/,
+                  resolve: {
+                    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+                  },
+                    use: {
+                        loader: 'babel-loader'
+                    },
+                    exclude: [/node_modules/],
+                },
+
+                {
+                    test:/\.scss$/,
+                    use:['style-loader', 
+                         'css-loader', 
+                         'sass-loader']
+                },
+                {
+                    test: /\.(c|sc|sa)ss$/,
+                    use: [
+                      'style-loader',
+                      'css-loader',
+                      {
+                        loader: 'postcss-loader',
+                        options: {
+                          postcssOptions: {
+                            plugins: [tailwindcss],
+                          },
+                        },
+                      },
+                      'sass-loader',
+                    ],
+                  },
+                ]
+    }
+
+}
